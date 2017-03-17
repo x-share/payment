c5e77e5caaf3f1473927d2083bff5bc04000ab78
lbl <1919305111@qq.com>
2017-03-18 01:49:31 +0800

    ---

--- Payment.Fuiou/Response/BasicPlain.cs
+++ Payment.Fuiou/Response/BasicPlain.cs
@@ -4,11 +4,6 @@
  * Description: 
  * ********************************/
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 
 namespace Payment.Fuiou.Response
